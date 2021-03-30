f9354cc5119777d8e19389fe62d8098e5694ba94
mobilesdk35 <mobilesdk35@gmail.com>
2021-03-30 13:21:27 +0300

    Version 5.17.0 released.

--- KandyLinkMobileSDKAnonymous.podspec
+++ KandyLinkMobileSDKAnonymous.podspec
@@ -1,24 +1,22 @@
 Pod::Spec.new do |s|
     s.name              = 'KandyLinkMobileSDKAnonymous'
-    s.version           = '5.16.0'
+    s.version           = '5.17.0'
     s.summary           = 'KandyLinkMobileSDKAnonymous'
     s.homepage          = 'https://github.com/Kandy-IO/'
     s.license 		      = { :type => 'MIT', :file => 'mit-license.txt'}
     s.author            = { 'Name' => 'cpaassdk@netas.com.tr' }
     s.platform          = :ios
-    s.source            = { :git => 'https://github.com/Kandy-IO/kandy-anonymous-ios-sdk.git', :tag => '5.16.0'}
+    s.source            = { :git => 'https://github.com/Kandy-IO/kandy-anonymous-ios-sdk.git', :tag => '5.17.0'}
     s.dependency  'KandyWebRTC'
-    s.ios.deployment_target   = '10.0'
+    s.ios.deployment_target   = '11.0'
     s.library           = ['c++','icucore','z']
     s.ios.frameworks    = ['AVFoundation','SystemConfiguration','GLKit','VideoToolbox','AudioToolbox','PushKit']
-    s.vendored_frameworks = ['MobileSDKAnonymous.framework', 'WebRTC.framework']
-    s.user_target_xcconfig = { 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'arm64' }
+    s.vendored_frameworks = ['MobileSDKAnonymous.xcframework']
     s.pod_target_xcconfig = {
         'ENABLE_BITCODE' => 'NO',
         'FRAMEWORK_SEARCH_PATHS' => '$(inherited)',
         'OTHER_LDFLAGS' => '$(inherited) -framework WebRTC -ObjC',
-        'CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES' => 'YES',
-        'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'arm64'
+        'CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES' => 'YES'
    }
 
 
---
+++ MobileSDKAnonymous.xcframework/ios-arm64/MobileSDKAnonymous.framework/Headers/SMCallServiceDelegate.h
@@ -0,0 +1,128 @@
+/*
+ * Copyright © 2014 GENBAND. All Rights Reserved.
+ *
+ * GENBAND CONFIDENTIAL. All information, copyrights, trade secrets
+ * and other intellectual property rights, contained herein are the property
+ * of GENBAND. This document is strictly confidential and must not be
+ * copied, accessed, disclosed or used in any manner, in whole or in part,
+ * without GENBAND's express written authorization.
+ *
+ * @version: 5.17.0
+ *
+ */
+
+#import <Foundation/Foundation.h>
+#import "SMOutgoingCallDelegate.h"
+
+@class SMMobileError;
+@class SMUriAddress;
+
+@protocol SMCallApplicationDelegate;
+
+/**
+ * @brief Call service methods
+ * @since 2.0.0
+ **/
+@protocol SMCallServiceDelegate <NSObject>
+
+/** If YES, MobileSDK will not initialize the audio unit automatically when an
+ *  audio track is ready for playout or recording. Instead, applications should
+ *  call setVoipAudioEnabled. If NO, MobileSDK will initialize the audio unit
+ *  as soon as an audio track is ready for playout or recording.
+ *  Default value is NO.
+ *
+ * @since 4.3.1
+ */
+@property(nonatomic) BOOL useManualAudio;
+
+/** This property is only effective if useManualAudio is YES.
+ *  Represents permission for MobileSDK to initialize the VoIP audio unit.
+ *  When set to NO, if the VoIP audio unit used by MobileSDK is active, it will be
+ *  stopped and uninitialized. This will stop incoming and outgoing audio.
+ *  When set to YES, MobileSDK will initialize and start the audio unit when it is
+ *  needed (e.g. due to establishing an audio connection).
+ *  Default value is NO.
+ *
+ * @since 4.3.1
+ */
+@property(nonatomic, getter=isVoipAudioEnabled) BOOL voipAudioEnabled;
+
+/**
+ * @brief Set the event handler CallApplicationDelegate for the CallService.
+ * @param app CallApplicationDelegate
+ * @since 2.0.0
+ */
+
+-(void) setCallApplication :(nonnull id <SMCallApplicationDelegate>) app;
+
+#pragma mark Regular Anonymous Call Create APIs
+
+/**
+ * @brief This method is to create outgoing/mobile originated calls
+ *
+ * @param app delegete of application to return callbacks for call
+ * @param terminator address of callee
+ * @param completion block
+ * @return call to establish call
+ * @since 3.1.4
+ */
+- (void) createOutGoingCall:(nonnull id <SMCallApplicationDelegate>)app
+              andTerminator:(nonnull SMUriAddress *)terminator
+                 completion:(nonnull void (^)(id<SMOutgoingCallDelegate> _Nullable call, SMMobileError * _Nullable error))callHandler;
+
+/**
+ * @brief This method creates anonymous calls
+ *
+ * @param app delegete of application to return callbacks for call
+ * @param account address defining the anonymous account
+ * @param originator address defining the source of the call
+ * @param terminator address defining the destination of the call
+ * @param completion block
+ * @return call to establish call
+ * @since 4.5.9
+ */
+- (void)createOutGoingCall:(nonnull id <SMCallApplicationDelegate>)app
+               withAccount:(nonnull SMUriAddress *)account
+             andOriginator:(nonnull SMUriAddress *)originator
+             andTerminator:(nonnull SMUriAddress *)terminator
+                completion:(nonnull void (^)(id<SMOutgoingCallDelegate> _Nullable call, SMMobileError * _Nullable error))callHandler;
+
+#pragma mark Time-Limited Token Based Anonymous Call Create API
+
+/**
+ * @brief This method creates anonymous calls with time-limited token
+ *
+ * @param app delegete of application to return callbacks for call
+ * @param accountToken encrypted address defining the anonymous account
+ * @param originatorToken encrypted address defining the source of the call
+ * @param terminatorToken encrypted address defining the destination of the call
+ * @param tokenRealm the realm used to encrypt the tokens
+ * @param completion block
+ * @return call to establish call
+ * @since 4.5.9
+ */
+- (void)createOutGoingCall:(nonnull id <SMCallApplicationDelegate>)app
+          withAccountToken:(nonnull NSString *)accountToken
+        andOriginatorToken:(nonnull NSString *)originatorToken
+        andTerminatorToken:(nonnull NSString *)terminatorToken
+             andTokenRealm:(nonnull NSString *)tokenRealm
+                completion:(nonnull void (^)(id<SMOutgoingCallDelegate> _Nullable call, SMMobileError * _Nullable error))callHandler;
+
+
+/**
+ * @brief Rotates camera to specified orientation
+ *
+ * @param orientation AVCaptureVideoOrientation value that will be used for camera orientation
+ * @since 2.0.0
+ */
+-(void) rotateCameraOrientationToPosition:(AVCaptureVideoOrientation) orientation;
+
+/**
+ * Returns the active call objects.
+ *
+ * @return Immutable call list
+ * @since 4.0
+ */
+- (nonnull NSArray<id<SMCallDelegate>> *) getActiveCalls;
+
+@end
